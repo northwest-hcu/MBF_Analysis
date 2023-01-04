f1fa1a7019e40602f997201888b9b8ea82eee079
北西 <northwest.output.official@gmail.com>
2023-01-05 07:12:42 +0900

    Update mbf_getInnerThreshold.m

--- mbf_getInnerThreshold.m
+++ mbf_getInnerThreshold.m
@@ -6,8 +6,10 @@ function selected_file_list = mbf_getInnerThreshold(file_list, threshold, center
         data = mbf_loadData(file_list{i, 1}, move_range); % データの読み込み
         if max(data.values) > threshold + center
             disp(strcat(file_list{i, 1}, ' is over upper threshold.'));
+            disp(strcat('max value is ', num2str(max(data.values))));
         elseif min(data.values) < (threshold * (-1)) + center
             disp(strcat(file_list{i, 1}, ' is over lower threshold.'));
+            disp(strcat('min value is ', num2str(min(data.values))));
         else
             selected_file_list{height(selected_file_list) + 1, 1} = file_list{i, 1};
         end
