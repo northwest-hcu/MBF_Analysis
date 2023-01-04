4a8b381a6f0e55ddf1c6a7e4fa0186a203fbaf14
北西 <northwest.output.official@gmail.com>
2023-01-05 06:49:52 +0900

    Update mbf_getInnerThreshold.m

--- mbf_getInnerThreshold.m
+++ mbf_getInnerThreshold.m
@@ -1,6 +1,7 @@
 function selected_file_list = mbf_getInnerThreshold(file_list, threshold, center, move_range)
     file_length = length(file_list);
-    selected_file_list = cell(0, 1); 
+    selected_file_list = cell(0, 1);
+    % ファイルごとに閾値を超えていなければ返り値に入れていく
     for i=1:file_length
         data = mbf_loadData(file_list{i, 1}, move_range); % データの読み込み
         if max(data.values) > threshold + center
