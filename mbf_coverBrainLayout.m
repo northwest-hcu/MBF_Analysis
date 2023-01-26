8fa5ef511cc12dc207c7ff64567ccce2623b9e0d
北西 <northwest.output.official@gmail.com>
2023-01-27 04:15:58 +0900

    update brain plot

--- mbf_coverBrainLayout.m
+++ mbf_coverBrainLayout.m
@@ -7,8 +7,10 @@ function points = mbf_coverBrainLayout(fig, points)
     [h, w, ~] = size(img);
     y_start = -0.55;
     y_end = 0.55;
-    x_start = y_start * (w / h);
-    x_end = y_end * (w / h);
+%     x_start = y_start * (w / h);
+%     x_end = y_end * (w / h);
+    x_start = -0.45;
+    x_end = 0.45;
     
     if points.flag
         plot(points.data(:, 1), points.data(:, 2), '.', 'MarkerSize', 1, 'MarkerFaceColor', 'none', 'MarkerEdgeColor', [0.5 0.5 0.5]);
@@ -19,7 +21,7 @@ function points = mbf_coverBrainLayout(fig, points)
             for j=1:w
                 if img(i, j, :) <= [200 200 200] % RGB value
                     y = (((y_end - y_start) / h) * i + y_start);% * (2 / pi);
-                    x = (((x_end - x_start) / w) * j + x_start);%* (2 / pi);
+                    x = (((x_end - x_start) / w) * j + x_start);% * (2 / pi);
                     data = zeros(1, 2);
                     data(1, 1) = x;
                     data(1, 2) = y;
--- mbf_coverHeadLayout.m
+++ mbf_coverHeadLayout.m
@@ -3,32 +3,18 @@ function points = mbf_coverHeadLayout(fig, points)
     figure(fig);
     hold on;
     % 顔の輪郭データを読み込み
-    layoutline1 = table2array(readtable('./headlayout1.csv'));
-    layoutline2 = table2array(readtable('./headlayout2.csv'));
-    layoutline3 = table2array(readtable('./headlayout3.csv'));
-    layoutline4 = table2array(readtable('./headlayout4.csv'));
+    layoutline1 = table2array(readtable('./layout_outline1.csv')); % MRI
+    layoutline2 = table2array(readtable('./layout_outline2.csv')); % 頭蓋骨
+    layoutline3 = table2array(readtable('./layout_outline3.csv')); % 鼻
+    layoutline4 = table2array(readtable('./layout_outline4.csv')); % 左耳
+    layoutline5 = table2array(readtable('./layout_outline5.csv')); % 右耳
     % 顔の輪郭をグラフに追加
     plot(layoutline1(:,1), layoutline1(:,2), 'k', 'LineWidth',3);
-    %plot(layoutline1(:,1), layoutline1(:,2), 'g', 'LineWidth',1, 'color', [222,184,135]./255);
     plot(layoutline2(:,1), layoutline2(:,2), 'k', 'LineWidth',3);
-    %plot(layoutline2(:,1), layoutline2(:,2), 'g', 'LineWidth',1, 'color', [222,184,135]./255);
     plot(layoutline3(:,1), layoutline3(:,2), 'k', 'LineWidth',3);
-    %plot(layoutline3(:,1), layoutline3(:,2), 'g', 'LineWidth',1, 'color', [222,184,135]./255);
     plot(layoutline4(:,1), layoutline4(:,2), 'k', 'LineWidth',3);
-    %plot(layoutline4(:,1), layoutline4(:,2), 'g', 'LineWidth',1, 'color', [222,184,135]./255);
-%     img = imread('./brain.png');
-%     img = flip(img);
-%     [h, w, ~] = size(img);
-%     img_alpha = ones(h, w, 3);
-%     for i=1:h
-%         for j=1:w
-%             if img(i, j, :) <= [230 230 230] % RGB value
-%                 img_alpha(i, j, :) = [0.4 0.4 0.4]; % transparent rate
-%             end
-%         end
-%     end
-% 
-%     image([-0.35 0.35]*(w/h), [-0.35, 0.35], img_alpha);
+    plot(layoutline5(:,1), layoutline5(:,2), 'k', 'LineWidth',3);
+
     points = mbf_coverBrainLayout(fig, points);
     xlim([-0.6 0.6]);
     ylim([-0.6 0.6]);
