83d1921ca3b68bcb39382b4bd160f17f7c918b48
北西 <northwest.output.official@gmail.com>
2023-02-01 11:33:17 +0900

    Update mbf_viewPositionMap.m

--- mbf_viewPositionMap.m
+++ mbf_viewPositionMap.m
@@ -7,4 +7,7 @@ function mbf_viewPositionMap(custom)
     for i=1:height(position_map)
         mbf_coverCoilLayout(fig, position_map(i, 2), position_map(i, 3), string(position_map(i, 1)));
     end
+    ax = gca;
+    ax.XTickLabel = cell(size(ax.XTickLabel));
+    ax.YTickLabel = cell(size(ax.YTickLabel));
 end
